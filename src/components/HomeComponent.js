f8ce9c745cde6a9642c5dffc8c320fa5588f37dc
jackshubh <shubham.sara@gmail.com>
2020-09-05 15:55:33 +0530

    React Router

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props){
+    return(
+        <div className="container">
+            <h4>Home</h4>
+            <p>Some content to show on Home Page.</p>
+        </div>
+    );
+}
+export default Home;
